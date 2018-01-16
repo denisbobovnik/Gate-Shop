38c274c15fa07e3bcc0a898981729d192973b69f
Denis Bobovnik <denis.bobovnik@gmail.com>
2018-01-16 07:18:09 +0100

    did a little bit of implementation for the testing part...

--- 3. Coding/RAW SOURCE/Denis/ProductDAO.java
+++ 3. Coding/RAW SOURCE/Denis/ProductDAO.java
@@ -1,12 +1,38 @@
 public class ProductDAO {
 
-	/**
-	 * 
-	 * @param product
-	 */
 	public void saveProduct(Product product) {
-		// TODO - implement ProductDAO.saveProduct
-		throw new UnsupportedOperationException();
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement ps=conn.prepareStatement("insert into products(products_id, name, description, preview) values (?,?,?,?)");
+			ps.setInt(1, product.getProductID());
+			ps.setString(2, product.getName());
+			ps.setString(3, product.getDescription());
+			ps.setString(4, product.getPreview());
+			ps.executeUpdate();
+
+		} finally {
+			if(conn!=null)
+			conn.close();
+		}
 	}
 
+	public Product selectProduct(int productID) {
+		Product ret = null;
+		Connection conn = DBConnect.getConnection();
+		try {
+			PreparedStatement ps=conn.prepareStatement("select * from products where products_id=?");
+			ps.setInt(1, productID);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				ret=new Product(rs.getInt("products_id"),rs.getString("name"),rs.getString("description"),rs.getString("preview"));
+				break;
+			}
+			rs.close();
+
+		} finally {
+			if(conn!=null)
+			conn.close();
+		}
+		return ret;
+	}
 }
